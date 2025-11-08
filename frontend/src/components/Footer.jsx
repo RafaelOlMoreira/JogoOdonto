e6b4657eb8232816d294d89e194cf5fd56aa89df
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2025-11-08 00:39:09 -0300

    refactor(frontend): Adjust Footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-            <footer className="mt-6 text-center text-xs text-gray-500">
+        <div className='bg-[#1f1f1f]'>
+            <footer className="space-y-6 text-center text-xs text-gray-500 border-t-1 border-gray-500">
                 Desenvolvido pela turma de odontologia — classifique corretamente o resíduo odontológico.
             </footer>
         </div>
